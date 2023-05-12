70bfef3de4e39b976c2e13e0db2f9f5b42beedff
ThomasF85 <thomas.foeldi@gmail.com>
2023-05-12 18:13:44 +0200

    add redux persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,22 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 import { useDispatch } from "react-redux";
 import store from "@/lib/store";
 import { useEffect } from "react";
 import { fetchPieces } from "@/lib/actions";
+const persistor = persistStore(store);
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <GlobalStyle />
       <Provider store={store}>
-        <Page Component={Component} {...pageProps} />
+        <PersistGate loading={null} persistor={persistor}>
+          <Page Component={Component} {...pageProps} />
+        </PersistGate>
       </Provider>
     </Layout>
   );
